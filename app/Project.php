0373a48afa963c6c73847fc207c039ab2f564c5a
antonmartsyniuk <terr83@gmail.com>
2018-11-06 19:11:21 +0200

    add Models & migrations

---
+++ app/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    // allowed statuses for project
+    private static $statuses = [
+        'planned',
+        'running',
+        'on hold',
+        'finished',
+        'cancel'
+    ];
+
+    // return allowed statuses
+    public static function getStatuses()
+    {
+        return self::$statuses;
+    }
+
+    protected $table = 'projects';
+}
